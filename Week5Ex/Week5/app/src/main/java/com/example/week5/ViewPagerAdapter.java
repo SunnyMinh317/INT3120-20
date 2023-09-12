a0b55b4806fb22babf177aaa9d7e3b79d2ac0baa
unknown <vunhatminh317@gmail.com>
2023-09-12 11:11:44 +0700

    Week 5 exercise

---
+++ Week5Ex/Week5/app/src/main/java/com/example/week5/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.week5;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position) {
+            case 0:
+                title="Clock";
+                break;
+            case 1:
+                title="Log In";
+                break;
+            case 2:
+                title="Home";
+                break;
+        }
+        return title;
+    }
+}
